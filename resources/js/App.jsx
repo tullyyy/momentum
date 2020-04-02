a893285d5e501441ae9340de0ebd5aadeebf358f
Tully <sehtully@gmail.com>
2020-04-02 12:38:12 +0200

    added opacity on background

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -4,42 +4,43 @@ import LinkToggle from "./components/Links/LinkToggle";
 import AboutToggle from "./components/About/AboutToggle";
 import getBackgroundImageURL from "./getBackgroundImageURL";
 
-import './style/style.css'
-import './style/clock.css'
-import './style/greeting.css'
-import './style/main-focus.css' 
-import './style/randomQuote.css'
+import "./style/style.css";
+import "./style/clock.css";
+import "./style/greeting.css";
+import "./style/main-focus.css";
+import "./style/randomQuote.css";
 
-import Clock from "./components/Clock"
-import Greeting from "./components/Greeting"
-import MainFocus from "./components/MainFocus"
-import RandomQuote from './components/RandomQuote'
+import Clock from "./components/Clock";
+import Greeting from "./components/Greeting";
+import MainFocus from "./components/MainFocus";
+import RandomQuote from "./components/RandomQuote";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.backgroundImage = getBackgroundImageURL();
-  }
+        this.backgroundImage = getBackgroundImageURL();
+    }
 
-  render() {
-    return (
-      <div
-        style={{ backgroundImage: `url("${this.backgroundImage}")` }}
-        className="main"
-      >
-        <LinkToggle />
-        <AboutToggle />
-        <TodoToggle />
-        <div className="App">
-        <Clock />
-        <Greeting />
-        <MainFocus />
-        <RandomQuote />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div
+                style={{ backgroundImage: `url("${this.backgroundImage}")` }}
+                className="main"
+            >
+                <div className="bg"></div>
+                <LinkToggle />
+                <AboutToggle />
+                <TodoToggle />
+                {/* <div className="App"> */}
+                <Clock />
+                <Greeting />
+                <MainFocus />
+                <RandomQuote />
+                {/* </div> */}
+            </div>
+        );
+    }
 }
 
 export default App;
